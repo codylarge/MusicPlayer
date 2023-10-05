403d8fdf3fca5878fd64db1db9efecf68f26bff4
Cody Large <dlarge1@ewu.edu>
2023-10-04 23:27:37 -0700

    Added Songs
    70ish songs

--- lastclickedsong.js
+++ lastclickedsong.js
@@ -9,18 +9,15 @@ document.addEventListener("DOMContentLoaded", function () {
   songList.addEventListener("click", function (e) {
     const target = e.target;
     if (target.tagName === "LI" && song.listNum != 0) {
-      // Remove the .last-clicked class from the previously clicked song item
       if (lastClickedSongItem) {
         lastClickedSongItem.classList.remove("last-clicked");
       }
 
-      // Update the last clicked song item
       lastClickedSongItem = target;
 
       // Add the .last-clicked class to the currently clicked song item
       lastClickedSongItem.classList.add("last-clicked");
 
-      // Update the h1 element with the last clicked song name
       currentSong.textContent = "Song: " + lastClickedSongItem.textContent;
     }
   });
